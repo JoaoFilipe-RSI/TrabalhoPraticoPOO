04e977901e06b8615cc442c9e96c08c4c6882518
JoaoFilipe-RSI <115697090+JoaoFilipe-RSI@users.noreply.github.com>
2024-10-18 10:08:23 +0100

    Add files via upload

---
+++ TrabalhoPratico-POO/Models/Morador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using TrabalhoPratico_POO.Enums;
+
+namespace TrabalhoPratico_POO.Models
+{
+    public class Morador
+    {
+        #region Private Properties
+        public int IdMorador { get; set; }
+        public TipoMorador Tipo { get; set; }
+        public string Nome { get; set; }
+        public string Apartamento { get; set; }
+        public string Contato { get; set; }
+        public Condominio Condominio { get; set; }
+        #endregion
+    }
+}
---
+++ TrabalhoPratico-POO/Services/CondominioService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TrabalhoPratico_POO.Models;
+using TrabalhoPratico_POO.Enums;
+
+namespace TrabalhoPratico_POO.Services
+{
+    public class CondominioService
+    {
+        private List<Condominio> condominios = new List<Condominio>();
+        private int idCounter = 1;
+
+        public void RegistarCondominio(string nome, string endereco, TipoCondominio tipo)
+        {
+            var condominio = new Condominio
+            {
+                IdCondominio = idCounter++,
+                Nome = nome,
+                Endereco = endereco,
+                Tipo = tipo
+            };
+            condominios.Add(condominio);
+            Console.WriteLine($"Condomínio {nome} do tipo {tipo} registado com sucesso!");
+        }
+
+        public void ListarCondominios()
+        {
+            Console.WriteLine("\nLista de Condomínios:");
+            foreach (var condominio in condominios)
+            {
+        Console.WriteLine($"ID: {condominio.IdCondominio}, Nome: {condominio.Nome}, Tipo: {condominio.Tipo}, Endereço: {condominio.Endereco}");
+        }
+        }
+
+        public Condominio ObterCondominioPorId(int id)
+        {
+            return condominios.FirstOrDefault(c => c.IdCondominio == id);
+        }
+    }
+}
